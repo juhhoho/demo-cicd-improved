c247cc33b90f6e6af1486a0e6ae351f92249ac14
junho <jh102328@khu.ac.kr>
2025-02-12 18:24:08 +0900

    Add HelloController.java

---
+++ src/main/java/_2024/winter/democicdimproved/hello/HelloController.java
@@ -0,0 +1,13 @@
+package _2024.winter.democicdimproved.hello;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> printHello(){
+        return ResponseEntity.ok().body("Hello, World!");
+    }
+}
