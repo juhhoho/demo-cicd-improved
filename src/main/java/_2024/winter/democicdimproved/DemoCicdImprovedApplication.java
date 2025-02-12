5e46fbcade09594e926e42425e5b265d59e778dc
junho <jh102328@khu.ac.kr>
2025-02-12 17:27:25 +0900

    init

---
+++ src/main/java/_2024/winter/democicdimproved/DemoCicdImprovedApplication.java
@@ -0,0 +1,13 @@
+package _2024.winter.democicdimproved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdImprovedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCicdImprovedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/_2024/winter/democicdimproved/DemoCicdImprovedApplicationTests.java
@@ -0,0 +1,13 @@
+package _2024.winter.democicdimproved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdImprovedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
