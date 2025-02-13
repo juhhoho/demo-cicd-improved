21b511a64da6e3a356431ee79c6de0e2d9d9e9d1
junho <jh102328@khu.ac.kr>
2025-02-13 17:06:24 +0900

    Add email service

---
+++ src/main/java/_2024/winter/democicdimproved/email/controller/EmailController.java
@@ -0,0 +1,30 @@
+package _2024.winter.democicdimproved.email.controller;
+
+
+import _2024.winter.democicdimproved.email.dto.CheckAuthEmailRequest;
+import _2024.winter.democicdimproved.email.dto.SendAuthEmailRequest;
+import _2024.winter.democicdimproved.email.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendAuthEmail(@RequestBody SendAuthEmailRequest request) {
+        return emailService.sendAuthEmail(request.getEmail());
+    }
+
+    @PostMapping("/check")
+    public ResponseEntity<String> checkAuthEmail(@RequestBody CheckAuthEmailRequest request) {
+        return emailService.checkAuthEmail(request.getEmail(), request.getAuthNum());
+    }
+}
---
+++ src/main/java/_2024/winter/democicdimproved/email/dto/SendAuthEmailRequest.java
@@ -0,0 +1,8 @@
+package _2024.winter.democicdimproved.email.dto;
+
+import lombok.Data;
+
+@Data
+public class SendAuthEmailRequest {
+    public String email;
+}
