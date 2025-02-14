01e30abb3650f848ff223dfa118a38224e831edd
junho <jh102328@khu.ac.kr>
2025-02-14 14:08:25 +0900

    update HelloController.java

--- src/main/java/_2024/winter/democicdimproved/hello/HelloController.java
+++ src/main/java/_2024/winter/democicdimproved/hello/HelloController.java
@@ -1,13 +1,16 @@
 package _2024.winter.democicdimproved.hello;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> printHello(){
+        log.info("[HelloController - printHello]");
         return ResponseEntity.ok().body("Hello, World! kkkk");
     }
     @GetMapping("/bye")
